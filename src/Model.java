441f320ba860361ae02c9a88fe026b3a95df3ba6
Tizzio <adriano.tummi@hotmail.it>
2017-07-08 19:49:23 +0200

    asd

--- src/Model.java
+++ src/Model.java
@@ -47,47 +47,5 @@ public class Model {
 		}
 	}
 
-	private void updateDb() throws ParseException {
-		try(Connection con = DriverManager.getConnection(url, user, password)){
-
-			// create tables
-			try(Statement st = con.createStatement()){
-				st.execute( "DROP TABLE IF EXISTS PAZIENTE" );
-				st.execute( "CREATE TABLE PAZIENTE (id SERIAL PRIMARY KEY, dataNascita DATE, name VARCHAR, age SMALLINT)" );
-				System.out.println( "Esito creazione tabella: " + st.getUpdateCount() );
-			} catch (SQLException e) {
-				System.out.println( "Problema durante creazione tabella: " + e.getMessage() );
-			}
-
-			//update tables
-			SimpleDateFormat sdf = new SimpleDateFormat( "dd/MM/yyyy" );
-			try(PreparedStatement pst = con.prepareStatement("INSERT INTO Paziente(dataNascita, name, age) VALUES (?,?,?),(?,?,?)")){
-				pst.clearParameters();
-
-				pst.setDate(1, new Date( sdf.parse("24/02/1993").getTime()));
-				pst.setString(2, "KARIM");
-				pst.setInt(3, 24);
-
-				pst.setDate(4, new Date( sdf.parse("16/12/1991").getTime()));
-				pst.setString(5, "ADRIANO");
-				pst.setInt(6, 25);
-
-				int n = pst.executeUpdate();
-				System.out.println( "Inserted " + n + " rows" );
-
-			}
-			catch (SQLException e) {
-				System.out.println( "Errore durante inserimento dati: " + e.getMessage() );
-				return;
-			}
-
-
-			// TODO: query database 
-			// ...
-
-		} catch (SQLException e1) {
-			System.out.println( "Errore durante connessione al database: " + e1.getMessage() );
-		}
-	}
-
+	 
 }
