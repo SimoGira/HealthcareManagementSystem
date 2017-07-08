8dc5e3cb7acfcb7ddcbc37f68e31fe15579ca208
Tizzio <adriano.tummi@hotmail.it>
2017-07-08 20:20:45 +0200

    added some queries in model class

--- src/Model.java
+++ src/Model.java
@@ -2,10 +2,12 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 public class Model {
 	private String url;
@@ -47,5 +49,97 @@ public class Model {
 		}
 	}
 
-	 
+	public void updateAmbulatory(Ambulatory amb)
+	{
+		try(Connection con = DriverManager.getConnection(url, user, password))
+		{ 
+			String query = "UPDATE ambulatory SET street = ?, cap = ?, city = ?, province = ?, contractDate = ?, description = ? WHERE name = ? AND company = ?";
+			try(PreparedStatement pst = con.prepareStatement(query)){
+				pst.clearParameters();
+				pst.setString(0, amb.street);
+				pst.setString(1, amb.cap);
+				pst.setString(2, amb.city);
+				pst.setString(3, amb.province);
+				pst.setDate(4, amb.contractDate);
+				pst.setString(5, amb.description);
+				pst.setString(6, amb.name);
+				pst.setString(7, amb.company);
+			}
+
+		} catch (SQLException e1) {
+			System.out.println( "Errore durante connessione al database: " + e1.getMessage() );
+		}
+	}
+
+	public ArrayList<Visit> getVisitsHistory(String patient)
+	{
+		try(Connection con = DriverManager.getConnection(url, user, password))
+		{
+			String query = "SELECT * FROM visit WHERE patient = ? AND result NOT IS NULL";
+			try(PreparedStatement pst = con.prepareStatement(query)){
+				pst.clearParameters();
+				pst.setString(0, patient); 
+				ResultSet rs = pst.executeQuery();
+				ArrayList<Visit> visits = new ArrayList<Visit>();
+				while(rs.next())
+				{
+					Visit v = new Visit();
+					v.ambulatoryName = rs.getString("ambulatory");
+					v.patient = rs.getString("patient");
+					v.ambulatoryName = rs.getString("ambulatory");
+					v.companyId = rs.getString("company");
+					v.serviceName = rs.getString("serviceName");
+					v.doctor = rs.getString("doctor");
+					v.date = rs.getDate("date");
+					v.hour = rs.getInt("hour");
+					v.urgency = rs.getString("urgency");
+					v.regime = rs.getString("regime");
+					v.result = rs.getString("result");
+					visits.add(v);
+				}
+				return visits;
+			} 
+
+		} catch (SQLException e1) {
+			System.out.println( "Errore durante connessione al database: " + e1.getMessage() );
+		}
+
+		return null;
+	}
+
+
+	public ArrayList<Ambulatory> getAmbulatories(String company)
+	{
+		try(Connection con = DriverManager.getConnection(url, user, password))
+		{
+			String query = "SELECT * FROM ambulatory WHERE company = ?";
+			try(PreparedStatement pst = con.prepareStatement(query)){
+				pst.clearParameters();
+				pst.setString(0, company); 
+				ResultSet rs = pst.executeQuery();
+				ArrayList<Ambulatory> ambulatories = new ArrayList<Ambulatory>();
+				while(rs.next())
+				{
+					Ambulatory a = new Ambulatory();
+					a.name = rs.getString("name");
+					a.company = rs.getString("company");
+					a.street = rs.getString("street");
+					a.cap = rs.getString("cap");
+					a.city = rs.getString("city");  
+					a.province = rs.getString("province");
+					a.contractDate = rs.getDate("contractDate");
+					a.description = rs.getString("description");
+					ambulatories.add(a);
+				}
+				return ambulatories;
+			} 
+
+		} catch (SQLException e1) {
+			System.out.println( "Errore durante connessione al database: " + e1.getMessage() );
+		}
+
+		return null;
+	}
+
+
 }
---
+++ src/Patient.java
@@ -0,0 +1,13 @@
+import java.sql.Date;
+
+public class Patient {
+    String fiscalcode;
+    String healthcarecompany;
+    String pin;
+    String name;  
+    String surname;
+    Date birthdate;
+    String birthplace; 
+    String province;
+    String email;
+}
