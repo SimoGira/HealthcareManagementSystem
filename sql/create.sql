92c8ec77bc8978a5a17fe2c2390e6e4767cfb3af
Tizzio <adriano.tummi@hotmail.it>
2017-07-09 02:50:46 +0200

    t

--- sql/create.sql
+++ sql/create.sql
@@ -5,9 +5,7 @@ create domain jobs as varchar(30)
 check(value in('dentista', 'segretario', 'infermiere','oculista', 'ortopedista', 'fisioterapista', 'cardiologo', 'dermatologo', 'oncologo', 'geriatra' ));
 
 create domain provinces as char(2)
-check(value in('AG','AL','AN','AO','AR','AP','AT','AV','BA','BT','BL','BN','BG','BI','BO','BZ','BS','BR','CA','CL','CB','CI','CE','CT','CZ','CH','CO','CS','CR','KR','CN','EN','FM','FE','FI','FG','FC','FR','GE','GO','GR','IM','IS','SP','AQ','LT','LE','LC','LI','LO','LU','MC','MN','MS','MT','ME','MI','MO','MB','NA','NO','NU','OT','OR','PD','PA','PR','PV','PG','PU','PE','PC','PI','PT','PN','PZ','PO','RG','RA','RC','RE','RI','RN','RM','RO','SA','VS','SS','SV','SI','SR','SO','TA','TE','TR','TO','OG','TP','TN','TV','TS','UD','VA','VE','VB','VC','VR','VV','VI','VT'));
-
--- "BL","PD","RO","TV","VE","VR","VI" forse nel dominio ci vanno solo queste
+check(value in('BL','PD','RO','TV','VE','VR','VI'));
 
 create table company(
     id varchar primary key,
