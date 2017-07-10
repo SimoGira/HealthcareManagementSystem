62b1ab30a9024f5090bdf71b52936e4ec2d858cc
Tizzio <adriano.tummi@hotmail.it>
2017-07-10 18:01:37 +0200

    asd

--- src/Clinic.java
+++ src/Clinic.java
@@ -58,4 +58,40 @@ public class Clinic {
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	
+	/*
+	 * 	private String name;
+	private String company;
+	private String street;
+	private String cap;
+	private String city;
+	private String province;
+	private Date contractDate;
+	private String description;
+	 */
+	
+	public String getCompleteDescription(String[] services){
+		
+		
+		String head =  "<table border=\"0\" cellpadding=\"2\" cellspacing=\"1\">"
+				+ "<tr>" + "<td align=\"right\"><b>" + "Nome Ambulatorio:" + "</b></td>" + "<td>" + name + "</td>" + "</tr>" 
+				+ "<tr>" + "<td align=\"right\"><b>" + "Indirizzo:" + "</b></td>" + "<td>" + street + "," + cap + " " + city + " (" + province + ")" + "</td>" + "</tr>"
+				+ "<tr>" + "<td align=\"right\"><b>" + "Data contratto:" + "</b></td>" + "<td>" + contractDate + "</td>" + "</tr>"  
+				+ "</table>"
+				+ "<p><hr/><b>Descrizione</b><hr/></p>"
+				+ description ;
+		
+
+		String svc = "<p><hr/><b>Services</b><hr/></p><ul>";
+		for(String s :services)
+			svc += "<li>" + s + "</li>";
+		svc += "</ul>";
+		
+		
+		return head + svc;
+		
+		
+		
+	}
+	
 }
